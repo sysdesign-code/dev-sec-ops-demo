badce703c6a30d0d6d49dcf19f2729ce402f32be
Nitin Vashishtha <vashi@google.com>
2022-08-05 09:20:00 -0400

    green deployment

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Please add one above. Blue</p>
+                <p className="text-center">No items yet! Please add one above. Green</p>
             )}
             {items.map(item => (
                 <ItemDisplay
